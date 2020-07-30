3f457754a7f8e2542a2a1f6efabeaef0df667f4b
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-07-30 17:44:44 +0300

    Add KeyboardEventHandlers to keys to make drumpad work from keyboard also

--- src/App.js
+++ src/App.js
@@ -6,32 +6,34 @@ import VolumeDown from '@material-ui/icons/VolumeDown'
 import VolumeUp from '@material-ui/icons/VolumeUp'
 import FormGroup from '@material-ui/core/FormGroup'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
+import KeyboardEventHandler from 'react-keyboard-event-handler'
 
 const App = () => {
   const [state, setState] = useState({ volume: 50, power: true, bank: false, displayText: "" })
+  const keys = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
 
   const sounds = {
     bankOneSounds: {
-      Q: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3", name: "Heater 1" },
-      W: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3", name: "Heater 2" },
-      E: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", name: "Heater 3" },
-      A: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3", name: "Heater 4" },
-      S: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3", name: "Clap" },
-      D: { url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3", name: "Open HH" },
-      Z: { url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3", name: "Kick-n' Hat" },
-      X: { url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3", name: "Kick" },
-      C: { url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3", name: "Closed HH" }
+      q: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3", name: "Heater 1" },
+      w: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3", name: "Heater 2" },
+      e: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", name: "Heater 3" },
+      a: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3", name: "Heater 4" },
+      s: { url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3", name: "Clap" },
+      d: { url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3", name: "Open HH" },
+      z: { url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3", name: "Kick-n' Hat" },
+      x: { url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3", name: "Kick" },
+      c: { url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3", name: "Closed HH" }
     },
     bankTwoSounds: {
-      Q: { url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3", name: "Chord 1" },
-      W: { url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3", name: "Chord 2" },
-      E: { url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3", name: "Chord 3" },
-      A: { url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3", name: "Shaker" },
-      S: { url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3", name: "Open HH" },
-      D: { url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3", name: "Closed HH" },
-      Z: { url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3", name: "Punchy Kick" },
-      X: { url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3", name: "Side Stick" },
-      C: { url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3", name: "Snare" }
+      q: { url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3", name: "Chord 1" },
+      w: { url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3", name: "Chord 2" },
+      e: { url: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3", name: "Chord 3" },
+      a: { url: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3", name: "Shaker" },
+      s: { url: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3", name: "Open HH" },
+      d: { url: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3", name: "Closed HH" },
+      z: { url: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3", name: "Punchy Kick" },
+      x: { url: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3", name: "Side Stick" },
+      c: { url: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3", name: "Snare" }
     }
   }
 
@@ -61,8 +63,8 @@ const App = () => {
   const buttonOnClick = (sound) => {
     if (state.power) {
       const bank = state.bank ? "bankTwoSounds" : "bankOneSounds"
-      setState({ ...state, displayText: sounds[bank][sound].name })
-      const audio = new Audio(sounds[bank][sound].url)
+      setState({ ...state, displayText: sounds[bank][sound.key].name })
+      const audio = new Audio(sounds[bank][sound.key].url)
       audio.volume = state.volume / 100
       audio.play()
     }
@@ -70,17 +72,14 @@ const App = () => {
 
   return (
     <div id="drum-machine">
+      {keys.map(key =>
+        <KeyboardEventHandler key={key} handleKeys={[key]} handleFocusableElements={true} onKeyEvent={(key, e) => buttonOnClick({ key })} />
+      )}
       <div className="box">
         <div className="drum-pad-container">
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("Q")}>Q</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("W")}>W</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("E")}>E</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("A")}>A</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("S")}>S</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("D")}>D</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("Z")}>Z</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("X")}>X</button>
-          <button className={state.power ? "drum-pad-power-on" : "drum-pad"} onClick={() => buttonOnClick("C")}>C</button>
+          {keys.map(key =>
+            <button className={state.power ? "drum-pad-power-on" : "drum-pad"} key={key} onClick={() => buttonOnClick({ key })}>{key.toUpperCase()}</button>
+          )}
         </div>
         <div className="controls-container">
           <div className="power-container">
@@ -91,7 +90,6 @@ const App = () => {
                   onChange={handlePowerChange}
                   color="primary"
                   width="50%"
-                  inputProps={{ 'aria-label': 'primary checkbox' }}
                 />}
                 label="Power"
                 labelPlacement="top"
